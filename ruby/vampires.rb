4fe7a2b9c8b34f00e48b896e6177373026f0ef5d
Tove Mercer <tovemercer@gmail.com>
2016-10-30 20:51:31 -0500

    added loop for number of applicants

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,59 +1,61 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many applicants do you have today?"
+applicants = gets.to_i
 
-if name == "Drake Cula" "Tu Fang"
-	name = false
-else
-	name = true
+
+
+until applicants == 0
+	puts "What is your name?"
+	name = gets.chomp
+	
+	puts "How old are you?"
+	age = gets.to_i
+	
+	puts "What year where you born?"
+	birth_year = gets.to_i
+	
+	current_year = 2016
+	birth_age = current_year - birth_year
+	
+	if birth_age == age
+		birth_age = true
+	else
+		birth_age = false
+	end
+	
+	puts "Do you like garlic bread? (y/n)"
+	garlic_bread = gets.chomp
+	
+	if garlic_bread == "y"
+		garlic_bread = true
+	elsif garlic_bread == "n"
+		garlic_bread = false
+	else
+		puts "Please put y or n"
+	end
+	
+	puts "Will you enroll in health insurance? (y/n)"
+	insurance = gets.chomp
+	
+	if insurance == "y"
+		insurance = true
+	elsif insurance == "n"
+		insurance = false
+	else
+		puts "Please put y or n"
+	end
+	
+	case
+	when name == "Drake Cula", name == "Tu Fang"
+		puts "Definitely a vampire."
+	when birth_age == false && garlic_bread == false && insurance == false
+		puts "Almost certainly a vampire."
+	when birth_age == true && (garlic_bread == true || insurance == true)
+		puts "Probably not a vampire."
+	when birth_age == false && (garlic_bread == false || insurance == false)
+		puts "Probably a vampire."
+	else puts "Results inconclusive."
+	end
+
+applicants -= 1
+
 end
-
-puts "How old are you?"
-age = gets.to_i
-
-puts "What year where you born?"
-birth_year = gets.to_i
-
-current_year = 2016
-birth_age = current_year - birth_year
-
-if birth_age == age
-	birth_age = true
-else
-	birth_age = false
-end
-
-puts "Do you like garlic bread? (y/n)"
-garlic_bread = gets.chomp
-
-if garlic_bread == "y"
-	garlic_bread = true
-elsif garlic_bread == "n"
-	garlic_bread = false
-else
-	puts "Please put y or n"
-end
-
-puts "Will you enroll in health insurance? (y/n)"
-insurance = gets.chomp
-
-if insurance == "y"
-	insurance = true
-elsif insurance == "n"
-	insurance = false
-else
-	puts "Please put y or n"
-end
-
-if name = false
-	puts "Definitely a vampire."
- elsif birth_age && (garlic_bread || insurance)
-	puts "Probably not a vampire."
-elsif birth_age && (garlic_bread || insurance)
-	puts "Probably a vampire."
-elsif birth_age && garlic_bread && insurance
-	puts "Almost certainly a vampire."
-
-else
-	puts "Results inconclusive."
-end
-
