6ac8c92112ba7d5a20316b1d0ecc33cacf8dc287
Tove Mercer <tovemercer@gmail.com>
2016-11-05 15:26:02 -0500

    let user update client info and printed the result

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -14,12 +14,12 @@
 #exit program
 
 puts "Welcome the the Interior Design Client Program!"	
-puts "Please enter the requested information below; when done type 'exit'."
+puts "Please enter the requested information below."
 
 puts "Client's name?"
 name = gets.chomp
 client_info = {
-	:client_name => "name"
+	:client_name => name
 }
 
 puts "Client's age?"
@@ -50,4 +50,65 @@ budget = gets.chomp
 		client_info[:client_budget] = false
 	end
 	
-puts client_info
+puts "Client's name: " + client_info[:client_name]
+puts "Client's age: " + client_info[:client_age]
+puts "Number of children in household: " + client_info[:number_of_children]
+puts "Prefered decor theme: " + client_info[:decor_theme]
+puts "Household pets? " + client_info[:household_pets].to_s
+puts "Budget over $5000? " + client_info[:client_budget].to_s
+
+puts "Do you need to update any of the stored information? (y/n)"
+update_information = gets.chomp
+
+if update_information == "y"
+	puts "What field would you like to update? Type: 'Name', 'Age', 'Children', 'Decor', 'Pets', 'Budget' or to exit 'none'."
+		field_update = gets.chomp
+		if field_update.downcase == "name"
+			puts "Client's name?"
+			name = gets.chomp
+			client_info[:client_name] = "name"
+		elsif field_update.downcase == "age"
+			puts "Client's age?"
+			age = gets.chomp
+			client_info[:client_age] = age
+		elsif field_update.downcase == "children"
+			puts "Number of children in household?"
+			children = gets.chomp
+			client_info[:number_of_children] = children
+		elsif field_update.downcase == "decor"
+			puts "Prefered decor theme?"
+			theme = gets.chomp
+			client_info[:decor_theme] = theme
+		elsif field_update.downcase == "pets"
+			puts "Do you have any pets? (y/n)"
+			pets = gets.chomp
+				if pets == "y"
+					client_info[:household_pets] = true
+				else 
+					client_info[:household_pets] = false
+				end
+		elsif field_update.downcase == "budget"
+			puts "Is your budget over $5000? (y/n)"
+			budget = gets.chomp
+				if budget == "y"
+					client_info[:client_budget] = true
+				else 
+					client_info[:client_budget] = false
+				end
+		elsif field_update.downcase == "none"
+			puts "Exiting program."
+			
+		else 
+			puts "What field would you like to update? Type: 'Name', 'Age', 'Children', 'Decor', 'Pets', 'Budget' or to exit 'none'."
+		end
+else 
+
+end
+
+puts "CLIENT INFORMATION:"
+puts "Name: " + client_info[:client_name]
+puts "Age: " + client_info[:client_age]
+puts "Number of children in household: " + client_info[:number_of_children]
+puts "Prefered decor theme: " + client_info[:decor_theme]
+puts "Household pets? " + client_info[:household_pets].to_s
+puts "Budget over $5000? " + client_info[:client_budget].to_s
