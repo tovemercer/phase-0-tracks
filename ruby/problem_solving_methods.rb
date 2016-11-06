3c721a11cabeb94dd33e730f9301e87f259237c5
Tove Mercer <tovemercer@gmail.com>
2016-11-06 17:06:58 -0600

    completed release 2, alphabet sort

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,5 +1,4 @@
 #Release 0
-#.while loop
 def search_array(arr, num)
 	int = 0
 	while int < arr.length
@@ -17,21 +16,6 @@ p search_array(arr, 3)
 p search_array(arr, 6)
 
 
-##.block
-#def search_array(arr, num)
-#	arr.each do |int|
-#		if arr[int] == num
-#			puts "#{int}"
-#		end #do
-#	end #while
-#end #def
-#
-#arr = [3, 6, 53, 12, 37]
-#p search_array(arr, 12)
-#p search_array(arr, 14)
-#p search_array(arr, 3)
-#p search_array(arr, 6)
-
 #Release 1
 def fibb(length)
 	arr = [0, 1] 
@@ -39,45 +23,49 @@ def fibb(length)
 		if arr.length != length
 			next_num = arr[-2] + arr[-1]
 			arr.push(next_num)
-		end
-	end
-end
+		end #if
+	end #do
+end #def
 
 fibb(100)
 
 #How can we verify this number is correct?
 #if fibb(100).index(101) == 218922995834555169026
 
-#Bubble Sort
-
-#def bubble_sort(array)
-#  n = array.length
-#  loop do
-#    swapped = false
-#
-#    (n-1).times do |i|
-#      if array[i] > array[i+1]
-#        array[i], array[i+1] = array[i+1], array[i]
-#        swapped = true
-#      end
-#    end
-#
-#    break if not swapped
-#  end
-#
-#  array
-#end
-#
-#array = [4, 2, 14, 10, 5, 100]		
-#bubble_sort(array)	
-
 #Release 2
 def alphabet_sort(array)
-	alphabet =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-	array.each do |letter|
-		if array[letter] > array[letter+1]
-			array[letter], array[letter+1] = array[letter+1], array[letter]
-			
-			
-		first_letter = array[0]
-		alphabet.index(first_letter)
+    alphabet =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
+    	"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+   counter = 1
+    until counter == 100
+
+    	new_array = array.each do |letter|
+    	
+    		i = 1
+    	    index_of_letter_in_array = array.index(letter)
+    	    index_of_next_letter_in_array = index_of_letter_in_array + i 
+    	    value_of_next_item_in_array = array[index_of_next_letter_in_array]
+    	   
+    	   until i == array[index_of_letter_in_array..-1].length
+    	   		    if 	alphabet.index(letter) > alphabet.index(value_of_next_item_in_array)
+	
+    	   				array[index_of_letter_in_array], array[index_of_next_letter_in_array] =
+    	   				 array[index_of_next_letter_in_array], array[index_of_letter_in_array]
+    	   			end #if
+    	   			
+    	   			i += 1 
+    	   	index_of_next_letter_in_array = index_of_letter_in_array + i 
+    	   	value_of_next_item_in_array = array[index_of_next_letter_in_array]
+    	   	end #until
+    	  	
+    	end #do
+
+    counter += 1
+	end #until
+	p new_array
+
+end #def
+
+p array_example = ["c", "e", "z", "a", "b", "l","m","a","x","y","i","h"]
+ 
+alphabet_sort(array_example)
