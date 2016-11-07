85778eed1999aa54f19c870472c99f0053dded29
Tove Mercer <tovemercer@gmail.com>
2016-11-07 09:18:26 -0600

    created loop for user input

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,18 +8,6 @@
 puts "What is your first and last name?"
 real_name = gets.chomp
 
-name_reorder = real_name.downcase.split(' ')
-order = [1, 0]
-order.map!{|x| name_reorder[x]}
-
-p name_reorder
-
-
-
-
-puts "What is your first and last name?"
-real_name = gets.chomp
-
 name_reorder = real_name.downcase.split('')
 #shuffle_name = name_reorder.shuffle
 #p shuffle_name.join
@@ -28,3 +16,35 @@ agent_name = name_reorder.map! {|x| x + "a"}
 p agent_name.join
 #new_alais = agent_name
 #puts new_alais
+
+
+
+
+
+def change_letters(name)
+	letters = "abcdefghijklmnopqrstuvwxyz".split("")
+	#letters = ["a", "e", "i", "o", "u"]
+	#next_letter = []
+	
+	letters.map! do |letters|
+		letters.next
+	end
+#p letters
+end
+
+#p letters
+
+puts "What is your first and last name? Or type 'exit' to leave the program."
+real_name = gets.chomp
+
+loop do
+	if real_name == "exit"
+		break
+	elsif real_name == ""
+		puts "Invalid entry. Please enter your first and last name or type 'exit' to leave the program."
+	else
+		p change_letters(real_name).join
+	end
+end
+
+		
