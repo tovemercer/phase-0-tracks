71b7e45bdd81ce639bb201938e62dc05fa57cd76
Tove Mercer <tovemercer@gmail.com>
2016-11-06 21:32:05 -0600

    added and 'a' to each letter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,7 +1,9 @@
 #ask the user for thier name
 #takes the name and swaps the first and last name
-#chnages the vowels in the name to the next vowel in 'aeiou'
- #changes the consonants to the next consonant in the alphabet
+#if a vowels ('aeiou') 
+	#converts them into the next vowel
+#if consonants 
+	#changes them into the next consonant in the alphabet
 
 puts "What is your first and last name?"
 real_name = gets.chomp
@@ -12,12 +14,17 @@ order.map!{|x| name_reorder[x]}
 
 p name_reorder
 
-#def secret_name(name_reorder)
-#	alphabet = "abcdefghijklmnopqrstuvwxyz"
-#	i = 0
-#	while i < name_reorder.length
-#	find_alphabet = alphabet.index(/[aeiou]/, name_reorder[i])
-#	name_reorder[i] = alphabet[find_alphabet + 1]
-#	i += 1
-#	end #while i
-#end #secret_name
+
+
+
+puts "What is your first and last name?"
+real_name = gets.chomp
+
+name_reorder = real_name.downcase.split('')
+#shuffle_name = name_reorder.shuffle
+#p shuffle_name.join
+#shuffle_name.class
+agent_name = name_reorder.map! {|x| x + "a"}
+p agent_name.join
+#new_alais = agent_name
+#puts new_alais
