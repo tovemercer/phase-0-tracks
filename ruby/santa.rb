8a118a44786cf397daae04310840589d044393d5
Tove Mercer <tovemercer@gmail.com>
2016-11-11 11:12:53 -0600

    created milk and cookie method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,7 +7,13 @@ def speak
 	puts "Ho, ho, ho! Haaaappy holidays!"
 end
 
+def eat_milk_and_cookies(cookie_type)
+	puts "That was a good #{cookie_type}!"
+	return cookie_type
+end
+
 end
 
 santa_mayhem = Santa.new
 santa_mayhem.speak
+santa_mayhem.eat_milk_and_cookies("chocolate chip")
