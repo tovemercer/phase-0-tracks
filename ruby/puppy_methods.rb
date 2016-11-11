10f5ba68862f0deb7ade020d6488f096f61f6ed4
Tove Mercer <tovemercer@gmail.com>
2016-11-11 14:04:16 -0600

    created puppy class and methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,37 @@ class Puppy
     toy
   end
 
+  def speak(int)
+  	i = 0
+  	while i < int do
+  		puts "Woof!"
+  	i += 1
+  	end
+  end
+
+  def roll_over
+	p "roll over"
+  end
+
+  def dog_years(human_years)
+  	$age = human_years * 7
+  	p $age
+  end
+
+  def high_five(num)
+  	i = 0
+  	while i < num do
+  		puts "Good dog!"
+  	i += 1
+  	end
+  end
+
 end
 
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak(5)
+puppy.roll_over
+puppy.dog_years(5)
+puts "Dogs age is #{$age}"
+puppy.high_five(3)
