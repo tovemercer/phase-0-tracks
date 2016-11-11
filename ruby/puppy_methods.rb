5ad8a1fb2a32561266c30e6813829199a7dd9b44
Tove Mercer <tovemercer@gmail.com>
2016-11-11 16:11:44 -0600

    looped through 50 instances of bird class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,5 +1,9 @@
 class Puppy
 
+  def initialize
+  	puts "Initializing new puppy instance ..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
@@ -39,3 +43,34 @@ puppy.roll_over
 puppy.dog_years(5)
 puts "Dogs age is #{$age}"
 puppy.high_five(3)
+
+class Birds
+
+   def initialize
+  	puts "Initializing new bird instance ..."
+   end
+
+   def food(type)
+   	puts "You ate a #{type}."
+   end
+
+   def miles_flown(int)
+   	puts "You flew #{int} miles!"
+   end
+
+end
+
+birddie_arr = []
+num = 0
+	
+	while num < 50
+	birddie_arr << Birds.new
+	num += 1
+	end
+
+birddie_arr.each do |bird_count|
+	bird_count.food("worm")
+	bird_count.miles_flown(100)
+end
+
+p birddie_arr
