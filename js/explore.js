53c0061aae114313384929c9f37c8d7d390e959f
Tove Mercer <tovemercer@gmail.com>
2016-11-15 14:03:02 -0600

    pseudocoded and wrote word reverse

---
+++ js/explore.js
@@ -0,0 +1,12 @@
+//declare a string (hello)
+//create loop to break apart the characters of the string
+//index the characters
+//use zero indexing to reverse the order
+//join the characters back together
+//reversed word (olleh)
+
+var str = "Hello"
+
+for (var i = str.length; i >= 0; i-=1) {
+	console.log(str[i]);
+}
