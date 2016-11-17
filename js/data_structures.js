cad693d90375f258609b1afb882a4a47c75c6e97
Tove Mercer <tovemercer@gmail.com>
2016-11-17 14:38:52 -0600

    created function

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,15 @@ for (var i = 0; i < colors.length; i ++) {
 }
 
 console.log(horses)
+
+function Car(make, model, price) {
+	this.make = make;
+	this.model = model;
+	this.price = price;
+	this.beep = function() { console.log("Beep!"); };
+}
+
+my_car = new Car("honda", "prelude", 2000)
+
+console.log(my_car)
+my_car.beep()
