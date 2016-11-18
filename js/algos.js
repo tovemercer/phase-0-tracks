35a3cec367804100b3592cbc8c3db6d40b1ab744
Tove Mercer <tovemercer@gmail.com>
2016-11-17 22:16:11 -0600

    created function that compare two objects for release 1

--- js/algos.js
+++ js/algos.js
@@ -1,31 +1,46 @@
-//takes an array of words or phrases
-//returns the longest word or phrase in the array
+//RELEASE 0
 
 //create array to store strings or phrases
-//	include phrases/words
-//sort through the phrases/words
-//reorder the list from greatest length to shortest
-//return sorted array (longest word/phase)
-
-
-//var words = ['hello', 'from', 'the', 'otherside']
-//
-//words.sort(function(a, b) {
-//	return a.length < b.length;
-//})
-//
-//
-//console.log(words)
-
-
 var words = []
 
+//test code:
 //words.push('hello', 'from', 'the', 'otherside')
 //words.push('send', 'my', 'love', 'to', 'your', 'new', 'lover')
 words.push('send my love', 'hello', 'when we were young')
 
+//sort through the phrases/words
+//reorder the list from greatest length to shortest
 words.sort(function(a, b) {
 	return a.length < b.length;
 })
 
+//return sorted array (longest word/phase)
 console.log(words)
+
+
+//RELEASE 1
+
+//function that takes 2 objects(hashes)
+//compare objects to see if they share at least 1 key/value pair
+
+//create function
+//set match to false
+//iterate through keys in object_1 and object_2
+//if match, change match to true
+var key_value = function(object_1, object_2) {
+	match = false;
+	for (var key_1 in object_1) {
+		for (var key_2 in object_2) {
+			if (key_1 === key_2 && object_1[key_1] === object_2[key_2]) {
+				match = true;
+			}
+		}
+	} 
+return match
+}
+
+console.log(key_value({name: "Bob", age: 20} , {name: "Tammy" , age: 18}));
+console.log(key_value({name: "Tammy", age: 20} , {name: "Tammy", age: 18}));
+
+//RELEASE 2
+
