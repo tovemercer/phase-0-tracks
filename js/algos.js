ae0172686d557240ed14356f053ea356ce7a3a6e
Tove Mercer <tovemercer@gmail.com>
2016-11-17 20:59:58 -0600

    sorted array from longest length to shortest

--- js/algos.js
+++ js/algos.js
@@ -3,7 +3,29 @@
 
 //create array to store strings or phrases
 //	include phrases/words
-//iterate through the phrases/words
+//sort through the phrases/words
 //reorder the list from greatest length to shortest
-//return 0 index (longest word/phase)
+//return sorted array (longest word/phase)
 
+
+//var words = ['hello', 'from', 'the', 'otherside']
+//
+//words.sort(function(a, b) {
+//	return a.length < b.length;
+//})
+//
+//
+//console.log(words)
+
+
+var words = []
+
+//words.push('hello', 'from', 'the', 'otherside')
+//words.push('send', 'my', 'love', 'to', 'your', 'new', 'lover')
+words.push('send my love', 'hello', 'when we were young')
+
+words.sort(function(a, b) {
+	return a.length < b.length;
+})
+
+console.log(words)
