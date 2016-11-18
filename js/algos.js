cb5d889e4fdd5b8b8c46ec666e85dda9a8ba41d1
Tove Mercer <tovemercer@gmail.com>
2016-11-17 18:49:12 -0600

    added pseudocode

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+//takes an array of words or phrases
+//returns the longest word or phrase in the array
+
+//create array to store strings or phrases
+//	include phrases/words
+//iterate through the phrases/words
+//reorder the list from greatest length to shortest
+//return 0 index (longest word/phase)
+
