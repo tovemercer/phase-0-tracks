6a25bae174dae8a1d8dfcee61d9ef93baafd0016
Tove Mercer <tovemercer@gmail.com>
2016-11-26 15:26:43 -0600

    pseudocoded interior design client info

---
+++ ruby/hashes_squirrels.rb
@@ -0,0 +1,14 @@
+#prompts user for client's information
+# => name(string)
+# => age (int)
+# => number of children (int)
+# => decor theme (string)
+# => housesold pets (boolean)
+# => stores info in a hash
+#prints the info to the user
+#asks user if there is any info that needs to updated
+#allow user to update information as needed
+# => if user enters 'none' skip to:
+#print the lastest version of the client's info
+#exit program
+
