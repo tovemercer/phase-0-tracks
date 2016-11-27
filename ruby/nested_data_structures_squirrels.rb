0309ba8f04f32d88af86cebb5e16b24439f05ab9
Tove Mercer <tovemercer@gmail.com>
2016-11-26 22:09:44 -0600

    nested data structure of music types, with bands and songs

---
+++ ruby/nested_data_structures_squirrels.rb
@@ -0,0 +1,44 @@
+# Release 1
+
+music = {
+	alternative: {
+		bands: [
+			"Nirvana",
+			"Pearl Jame",
+			"Alice in Chains"
+			],
+
+		songs: [
+			"Smells like Teen Spirit",
+			"Jeremy",
+			"Man in the Box"
+		]
+	},
+
+	indie: {
+		bands: [
+			"Vampire Weekend",
+			"Tame Impala",
+			"Ra Ra Riot"
+			],
+
+		songs: [
+			"Ya Hey",
+			"The Less I Know the Better",
+			"Water"
+		]
+	},
+
+	eighties: {
+		bands: [
+			"Hall and Oats",
+			"Kenny Loggins",
+			"Michael McDonald"
+			],
+
+		songs: [
+			"I Can't Go for That",
+			"Footloose",
+			"What a Fool Believes"]
+	}
+}
