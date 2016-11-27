faf10a0472889048125faaeb2cb09286b9a56d57
Tove Mercer <tovemercer@gmail.com>
2016-11-27 17:22:54 -0600

    release 1- fibonacci numbers

--- ruby/problem_solving_methods_squirrels.rb
+++ ruby/problem_solving_methods_squirrels.rb
@@ -1,3 +1,4 @@
+# Release 0
 def search_array(arr, num)
 	int = 0
 	while int < arr.length
@@ -13,3 +14,22 @@ arr = [42, 89, 23, 1]
 
 search_array(arr, 5)
 search_array(arr, 89)
+
+# Release 1
+def fibb(length)
+	arr = [0, 1]
+	arr.each do |number|
+		if arr.length != length
+			next_num = arr[-2] + arr[-1]
+			arr.push(next_num)
+		end
+	end
+end
+
+#fibb(100)
+if fibb(100).last == 218922995834555169026
+	puts "Good work!"
+else
+	puts "Almost there!"
+end
+
