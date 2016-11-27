dbb4075b03afad5ecda735f83a2674a559758f9d
Tove Mercer <tovemercer@gmail.com>
2016-11-26 21:00:58 -0600

    completed release 1; using .each and .map on hashes and arrays

--- ruby/iteration_squirrels.rb
+++ ruby/iteration_squirrels.rb
@@ -1,6 +1,50 @@
-music_genres = ['indie', 'hip-hop', 'alternative']
-musician_songs = {
-	vampire_weekend: "step",
-	kendrick_lamar: "king kunta",
-	pearl_jam: "black",
+holidays = ['thanksgiving', 'christmas', 'new years']
+thanksgiving_dinner = {
+	protien: "turkey",
+	veggie: "green beans",
+	desert: "apple pie"
 }
+
+## Iterate using .each
+# Array
+
+puts "Before .each call:"
+puts holidays
+puts
+
+holidays.each do |holiday|
+	holiday
+end
+
+puts "After .each call:"
+puts holidays
+puts
+
+# Hash
+
+puts "Before .each call:"
+puts thanksgiving_dinner
+puts
+
+thanksgiving_dinner.each do |food_type, food_item|
+	puts "We ate #{food_item} as our #{food_type} on Thanksgiving."
+end
+
+puts
+
+## Iterate using .map!
+# Array
+
+puts "Before .map! call:"
+puts holidays
+puts
+
+holidays.map! do |holiday|
+	holiday.reverse
+end
+
+puts "After .map! call:"
+puts holidays
+puts
+
+
