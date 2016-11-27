7f6d00f7004d2b861e949a92aea066d5caac0d3c
Tove Mercer <tovemercer@gmail.com>
2016-11-26 21:33:33 -0600

    used documentation to find method calls for arrays and hashes for release 2

--- ruby/iteration_squirrels.rb
+++ ruby/iteration_squirrels.rb
@@ -47,4 +47,47 @@ puts "After .map! call:"
 puts holidays
 puts
 
+## Block Methods
+# Array
 
+# 1
+number = [3, 66, 14, 78, 32, 7]
+
+number.delete_if { |x| x < 29 }
+
+# 2
+number = [3, 66, 14, 78, 32, 7]
+
+number.keep_if { |x| x < 29 }
+
+# 3
+number = [3, 66, 14, 78, 32, 7]
+
+number = number.index { |x| x == 7}
+
+# 4
+number = [3, 66, 14, 78, 32, 7]
+
+number = number.drop_while { |i| i < 75 }
+
+# Hash
+
+# 1
+hashy = {a: 1, b: 2, c: 3, d: 4, e: 5}
+
+hashy.delete_if { |letter, number| number >= 4}
+
+# 2
+hashy = {a: 1, b: 2, c: 3, d: 4, e: 5}
+
+hashy.keep_if { |letter, number| number >= 4}
+
+# 3
+hashy = {a: 1, b: 2, c: 3, d: 4, e: 5}
+
+hashy.has_key?(:e)
+
+#4
+hashy = {a: 1, b: 2, c: 3, d: 4, e: 5}
+
+hashy.reject { |letter, number| number > 3 }
