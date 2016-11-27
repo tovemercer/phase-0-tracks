bf93968361c7e585fec5e8a56db7111a5030addb
Tove Mercer <tovemercer@gmail.com>
2016-11-27 17:57:20 -0600

    alphabetically sorted an array for release 2

--- ruby/problem_solving_methods_squirrels.rb
+++ ruby/problem_solving_methods_squirrels.rb
@@ -33,3 +33,35 @@ else
 	puts "Almost there!"
 end
 
+# Release 2
+def alphabet_sort(array)
+  alphabet =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
+    	"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  counter = 0
+  until counter == 50
+
+    new_array = array.each do |letter|
+      i = 1
+      letter_index = array.index(letter)
+      next_letter_index = letter_index + i 
+      next_letter = array[next_letter_index]
+
+        until i == array[letter_index..-1].length
+          if 	alphabet.index(letter) > alphabet.index(next_letter)
+          array[letter_index], array[next_letter_index] = array[next_letter_index], array[letter_index]
+          end
+
+        i += 1 
+        next_letter_index = letter_index + i 
+        next_letter = array[next_letter_index]
+      end
+
+    end
+  counter += 1
+  end 
+p new_array
+end
+
+p array_example = ["t", "e", "a", "f", "h", "g", "q", "l", "s", "r", "v", "k"]
+ 
+alphabet_sort(array_example)
