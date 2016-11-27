cd7e7226201a7d97f5e70faa9fb463a4ab24a4b3
Tove Mercer <tovemercer@gmail.com>
2016-11-27 16:25:47 -0600

    created alias generator to create new name

--- ruby/alias_manager_squirrels.rb
+++ ruby/alias_manager_squirrels.rb
@@ -12,9 +12,9 @@ def next_vowel(vowel)
 	i = 0
 	vowels = "aeiou"
 		while i < vowel.length
-			ltr = vowels.index(vowel[i])
-			ltr += 1
-			 vowel[i] = vowels[ltr]
+			v_ltr = vowels.index(vowel[i])
+			v_ltr += 1
+			vowel[i] = vowels[v_ltr]
 			i += 1
 		end
    return vowel
@@ -25,13 +25,38 @@ def next_consonant(consonant)
 	i = 0
 	consonants = "bcdfghjklmnpqrstvwxyz"
 		while i < consonant.length
-			ltr = consonant.index(consonant[i])
-			ltr += 1
-			consonant[i] = consonants[i]
+			c_ltr = consonant.index(consonant[i])
+			c_ltr += 1
+			consonant[i] = consonants[c_ltr]
 			i += 1
 		end
 	return consonant
 end
 
-next_vowel('a')
-next_consonant('b')
+#next_vowel('a')
+#next_consonant('b')
+
+#def name_switch(real_name)
+#	name_flip = real_name.downcase.split(' ')
+#	name_flip.reverse_each { |i| print i, '' }
+##	letters = name_flip.to_s.split('')
+#end
+
+def alias_generator(name)
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+new_name = []
+  name.downcase.each_char do |ltr|
+  	if vowels.include?(ltr)
+  		new_name << next_vowel(ltr)
+  	else consonants.include?(ltr)
+  		new_name << next_consonant(ltr)
+  	end
+  end
+new_name.join
+end
+
+p alias_generator("Felicia Torres") # => "Vussit Gimodoe"
+
+
+
