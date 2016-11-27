616100cee5fdf10df0b1f041d42c816115e885ef
Tove Mercer <tovemercer@gmail.com>
2016-11-27 17:10:05 -0600

    searched array for release 0

--- ruby/problem_solving_methods_squirrels.rb
+++ ruby/problem_solving_methods_squirrels.rb
@@ -1,6 +1,15 @@
 def search_array(arr, num)
-	
+	int = 0
+	while int < arr.length
+		if arr[int] == num
+			puts int
+		end
+	int = int + 1
+	end
+end
 
 
 arr = [42, 89, 23, 1]
 
+search_array(arr, 5)
+search_array(arr, 89)
