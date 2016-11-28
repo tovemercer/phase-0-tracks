92fe9fedc5d25fba9a054d1fac0b5eed4137b0b9
Tove Mercer <tovemercer@gmail.com>
2016-11-28 09:43:05 -0600

    uodated puppy class for release 1

---
+++ ruby/puppy_squirrels.rb
@@ -0,0 +1,14 @@
+SPECIES (Class)------------------------
+Cutie Furryness
+
+CHARACTERISTICS (Attributes) ----------------
+Fur length: varies
+Friendly: yes
+Tail count: 1
+Name: varies
+Color: varies
+
+BEHAVIOR (Methods)-----------------------
+Play
+Eat
+Sleep
