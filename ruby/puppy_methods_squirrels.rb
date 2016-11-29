cd11d2fc3bfb4c9d818563d8d507cf97fdd7a44b
Tove Mercer <tovemercer@gmail.com>
2016-11-29 16:25:08 -0600

    completed methods for release 0

---
+++ ruby/puppy_methods_squirrels.rb
@@ -0,0 +1,38 @@
+class Puppy
+
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+
+  def speak(int)
+  	i = 0
+  	while i < int
+  		puts "Woof!"
+  		i += 1
+  	end
+  end
+
+  def roll_over
+		p "roll over"
+  end
+
+  def dog_years(human_years)
+  	$age = human_years * 7
+  	p $age
+  end
+
+  def attack(name)
+  	puts "Nevermind, I'd rather just sleep."
+  end
+
+			
+end
+
+
+puppy = Puppy.new
+puppy.fetch('ball')
+puppy.speak(4)
+puppy.roll_over
+puppy.dog_years(7)
+puppy.attack("Chris")
