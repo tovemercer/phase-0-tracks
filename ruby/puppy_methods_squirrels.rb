576e9627d6fc7c4a91b6a080757228b742637be8
Tove Mercer <tovemercer@gmail.com>
2016-11-29 16:35:33 -0600

    created Cat class for release 2

--- ruby/puppy_methods_squirrels.rb
+++ ruby/puppy_methods_squirrels.rb
@@ -40,3 +40,34 @@ puppy.speak(4)
 puppy.roll_over
 puppy.dog_years(7)
 puppy.attack("Chris")
+
+
+class Cat
+
+	def initialize
+		puts "I'm the new cat on the block!"
+	end
+
+	def play(toy)
+		puts "I attacked the #{toy}."
+	end
+
+	def sleep(hours)
+		puts "The cat slept #{hours} hours, but is still tired."
+	end
+
+	def speak(int)
+		p "Meow!" * int
+	end
+
+end
+
+kitty = Cat.new
+kitty.play('feather')
+kitty.sleep(5)
+kitty.speak(8)
+
+
+
+
+
