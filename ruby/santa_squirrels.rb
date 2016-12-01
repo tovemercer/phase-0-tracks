36298358f7d3d9ddcef4c3cf659dd5f5549df596
Tove Mercer <tovemercer@gmail.com>
2016-11-30 20:55:55 -0600

    updated class with attributes, iterated through driver code to create multiple santas

--- ruby/santa_squirrels.rb
+++ ruby/santa_squirrels.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 	
 	def speak
@@ -14,6 +18,21 @@ class Santa
 
 end
 
-santas = Santa.new
-santas.speak
-santas.eat_milk_and_cookies('chocolate chip')
+#santas = Santa.new
+#santas.speak
+#santas.eat_milk_and_cookies('chocolate chip')
+
+santas = []
+genders = ["Male", "Female", "Bigender", "Gender Fulid", "Agender", "Female"]
+ethnicities = ["Black", "Hispanic", "White", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)"]
+
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicities[i])
+  puts "#{santas.length} santas have been added."
+  puts
+end
+
+santas.each do |santa|
+	santa.speak
+	santa.eat_milk_and_cookies("peanut butter")
+end
