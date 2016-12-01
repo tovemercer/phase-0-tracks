2f03a2d0a720e617d97260abbc95b3fb5656feb8
Tove Mercer <tovemercer@gmail.com>
2016-12-01 10:44:00 -0600

    shout mixin, and classes that call mixin

--- ruby/shout_squirrels.rb
+++ ruby/shout_squirrels.rb
@@ -1,12 +1,32 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + ":("
-	end
+#module Shout
+#	def self.yell_angrily(words)
+#		words + "!!!" + ":("
+#	end
+#
+#	def self.yell_happily(words)
+#		words + "!!!" + ":)"
+#	end
+#end
+#
+#puts Shout.yell_angrily("Boo")
+#puts Shout.yell_happily("Yay")
 
-	def self.yell_happily(words)
-		words + "!!!" + ":)"
+module Shout
+	def talking_loudly(word)
+		puts "When I get excited I shout, #{word}!"
 	end
 end
 
-puts Shout.yell_angrily("Boo")
-puts Shout.yell_happily("Yay")
+class Coach
+	include Shout
+end
+
+class Doctor
+	include Shout
+end
+
+coach = Coach.new
+coach.talking_loudly("goal")
+
+doctor = Doctor.new
+doctor.talking_loudly("damnit, I'm a doctor not a robot")
