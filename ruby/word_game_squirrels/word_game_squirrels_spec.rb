17c3dcefd2209a0d9aa884c59a7a7cbf7fa3cbb2
Tove Mercer <tovemercer@gmail.com>
2016-12-04 23:15:42 -0600

    all tests are green

--- ruby/word_game_squirrels/word_game_squirrels_spec.rb
+++ ruby/word_game_squirrels/word_game_squirrels_spec.rb
@@ -3,29 +3,28 @@ require_relative 'word_game_squirrels'
 describe WordGame do
 	let(:game) { WordGame.new("snow") }
 
-it "adds the guessed letter to guessed letter array" do
-	game.game_word("g")
-	expect(game.guessed_letter).to include ("g")
-end
+	it "takes a guess and returns boolean if letter is in the word" do
+		expect(game.game_word("w")).to eq 1
+		expect(game.game_word("a")).to eq 0
+	end
 
-it "takes a guess and returns index if letter is in the word" do
-	expect(game.game_word("w")).to eq 3 #failed
-end
-
-# this one passed!
-it "checks to see is the letter has already been guesses" do
-	expect(game.previous_guess("l")).to eq false
-	game.game_word("l")
-	expect(game.previous_guess("l")).to eq true
-end
-
-it "if gameboard is equal to game word, returns winner" do
-	expect(game.winner("hello")).to eq false #passed
-	expect(game.winner("snow")).to eq true #failed
-end
-
-#game_word
-#player_guess
-#winner
+	it "subtracts one guess from the word length" do
+		expect(game.player_guess("n")).to eq 3
+	end
+	
+	it "checks to see is the letter has already been guesses" do
+		expect(game.previous_guess("l")).to eq false
+		game.game_word("l")
+		expect(game.previous_guess("l")).to eq true
+	end
+	
+	it "if gameboard is equal to game word, returns winner" do
+		expect(game.winner).to eq false
+		game.game_word("s")
+		game.game_word("n")
+		game.game_word("o")
+		game.game_word("w")
+		expect(game.winner).to eq true
+	end
 
 end
