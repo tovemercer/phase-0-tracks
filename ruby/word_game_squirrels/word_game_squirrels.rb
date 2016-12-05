d99677905ecc34a625517c383018abe9c6ad2bec
Tove Mercer <tovemercer@gmail.com>
2016-12-04 23:17:48 -0600

    un-commented out the user interface

--- ruby/word_game_squirrels/word_game_squirrels.rb
+++ ruby/word_game_squirrels/word_game_squirrels.rb
@@ -36,44 +36,44 @@ class WordGame
 
 end
 
-# USER INTERFACE ##
-#puts "Welcome to the Word Game!"
-#puts "Here's how it works... Player 1 enters a word for Player 2 to guess." 
-#puts "Easy enough, right? Let's get started!"
-#puts "\nPlayer 1, enter a word."
-#game_word = gets.chomp.downcase
-#game = WordGame.new(game_word)
-#
-#puts "\nPlayer 2, you have #{game.guesses} chances to guess Player 1's word. Good Luck!"
-#puts "Guess this word: #{game.gameborad.join}"
-#puts "\nPlease enter your first letter."
-#
-#while game.guesses > 0
-#	guess = gets.chomp.downcase
-#
-#		if game.previous_guess(guess)
-#			puts "You've already tried that one... Enter a new letter."
-#			guess = gets.chomp.downcase
-#		end
-#
-#	gameboard_update = game.game_word(guess)
-#	correct_guess = gameboard_update > 0
-#
-#		if game.gameborad.include? guess
-#			puts "Good guess!"
-#			puts "You have #{game.guesses} guesses left."
-#			puts game.gameborad.join
-#		else
-#			puts "Better try again..."
-#			puts "You have #{game.guesses} guesses left."
-#			puts game.gameborad.join
-#		end
-## can't figure out how to make these only display when the game is over
-#		if game.winner
-#			puts "You guessed the word, hooray!!! Your mother must be very proud."
-#			break
-#		else !game.winner
-#			puts "Well... Better luck next time."
-#		end
-#
-#end
+## USER INTERFACE ##
+puts "Welcome to the Word Game!"
+puts "Here's how it works... Player 1 enters a word for Player 2 to guess." 
+puts "Easy enough, right? Let's get started!"
+puts "\nPlayer 1, enter a word."
+game_word = gets.chomp.downcase
+game = WordGame.new(game_word)
+
+puts "\nPlayer 2, you have #{game.guesses} chances to guess Player 1's word. Good Luck!"
+puts "Guess this word: #{game.gameborad.join}"
+puts "\nPlease enter your first letter."
+
+while game.guesses > 0
+	guess = gets.chomp.downcase
+
+		if game.previous_guess(guess)
+			puts "You've already tried that one... Enter a new letter."
+			guess = gets.chomp.downcase
+		end
+
+	gameboard_update = game.game_word(guess)
+	correct_guess = gameboard_update > 0
+
+		if game.gameborad.include? guess
+			puts "Good guess!"
+			puts "You have #{game.guesses} guesses left."
+			puts game.gameborad.join
+		else
+			puts "Better try again..."
+			puts "You have #{game.guesses} guesses left."
+			puts game.gameborad.join
+		end
+# can't figure out how to make these only display when the game is over
+		if game.winner
+			puts "You guessed the word, hooray!!! Your mother must be very proud."
+			break
+		else !game.winner
+			puts "Well... Better luck next time."
+		end
+
+end
