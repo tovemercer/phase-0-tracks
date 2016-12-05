158076552f184180128612d75db6002723393d82
Tove Mercer <tovemercer@gmail.com>
2016-12-04 20:50:43 -0600

    running through spec file with code

--- ruby/word_game_squirrels/word_game_spec_squirrels.rb
+++ ruby/word_game_squirrels/word_game_spec_squirrels.rb
@@ -1,5 +1,10 @@
 require_relative 'word_game_squirrels'
 
 describe WordGame do
-	let(:game) { WordGame.new }
+	let(:game) { WordGame.new("snow") }
+
+it "takes a guess and returns boolean if letter is in the word" do
+	expect(game.game_word("w")).to eq 1
+end
+
 end
--- ruby/word_game_squirrels/word_game_squirrels.rb
+++ ruby/word_game_squirrels/word_game_squirrels.rb
@@ -1,3 +1,23 @@
 class WordGame
 
+def initialize(game_word)
+	@game_word = game_word.split("")
+	@guessed_letters = []
+	@guesses = game_word.length
+end
+
+def game_word(ltr)
+	int = 0
+	@game_word.each_index do |i|
+		if @game_word[i] == ltr
+			@game_word[i] = ltr
+			int += 1
+		end
+	end
+	@guessed_letters << ltr
+	@guesses -= 1
+	return int
+end
+
+
 end
