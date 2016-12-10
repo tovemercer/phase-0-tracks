999e48d348acac9dea48179bb55fa222be76296c
Tove Mercer <tovemercer@gmail.com>
2016-12-10 13:31:36 -0600

    reversed the order of a word

--- js/explore_squirrels.js
+++ js/explore_squirrels.js
@@ -1,6 +1,15 @@
-//string is set as a variable
-//iterate through variable
-//reverse the order
-//join the characters back together
-//prints reversed word
+//string is passed in as an argument
+//create empty variable to store reversed word
+//iterate through passes in word backwards using neg integers
+//  characters are stored (in reverse) in the empty variable
+//returns reversed word
 
+function reverse(str) {
+  var result = "";
+  for (var i = str.length - 1; i>= 0; i --) {
+    result += str[i];
+  }
+  return result
+}
+
+console.log(reverse("hello"))
