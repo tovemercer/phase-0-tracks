5d14e5ef6479948d910ecdebc22aa7fe50e40807
Tove Mercer <tovemercer@gmail.com>
2016-12-12 09:35:50 -0600

    function to find a key-value match

--- js/algos_squirrels.js
+++ js/algos_squirrels.js
@@ -14,3 +14,26 @@ theDoors.sort(function(a, b) {
 
 //return sorted array (longest word/phase)
 console.log(theDoors)
+
+
+//RELEASE 1
+
+//function that takes 2 objects(hashes)
+//compare objects to see if they share at least 1 key/value pair
+
+//create function
+//set match to false
+//iterate through keys in object_1 and object_2
+//if match, change match to true
+var keyValue = function(object_1, object_2) {
+  match = false;
+  for (var key in object_1) {
+    if(object_1[key] == object_2[key]) {
+      return true
+    }
+  } 
+return match
+}
+
+console.log(keyValue({name: "Steven", age: 54} , {name: "Tamir" , age: 54}));
+console.log(keyValue({name: "Steven", age: 45} , {name: "Tamir", age: 54}));
